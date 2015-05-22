8bbe63f915d6f08e3559d73264d69f3769179c78
diabolique- <gillen314@gmail.com>
2015-05-22 16:10:47 -0500

    initial commit
    All that exists now is an __init__ script that runs when imported that
    changes a few matplotlib parameters to make for a slightly nicer plot.
    Also defines a nearly black color that is easier on the eyes.

---
+++ __init__.py
@@ -0,0 +1,17 @@
+from palettable.tableau import Tableau_10
+import matplotlib as mpl
+
+almost_black = '#262626'
+
+mpl.rcParams['axes.color_cycle'] = Tableau_10.hex_colors
+mpl.rcParams['legend.scatterpoints'] = 1
+mpl.rcParams['savefig.format'] = 'pdf'
+mpl.rcParams['font.family'] = 'sans-serif'
+mpl.rcParams['font.sans-serif'] = 'Helvetica'
+mpl.rcParams['patch.edgecolor'] = almost_black
+mpl.rcParams['text.color'] = almost_black
+mpl.rcParams['axes.edgecolor'] = almost_black
+mpl.rcParams['axes.labelcolor'] = almost_black
+mpl.rcParams['xtick.color'] = almost_black
+mpl.rcParams['ytick.color'] = almost_black
+mpl.rcParams['grid.color'] = almost_black
