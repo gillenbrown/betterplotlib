9441fa78151fd7782fc76adbf249add794b8ff85
diabolique- <gillen314@gmail.com>
2015-06-23 15:07:24 -0500

    new bold font, new color
    I was inspired by the national park road signs in Canada to make the
    font bold and Helvetica Neue, and added the color of the road signs
    there. It’s a really nice green.

--- colors.py
+++ colors.py
@@ -1,3 +1,10 @@
 almost_black = '#262626'
 light_gray = '#E5E5E5'
 steel_blue = '#3F5D7D'
+# I saw the national park road signs and loved the color, so I found it. Here
+# is the source if you want (PDF):
+# http://www.pc.gc.ca/eng/docs/bib-lib/~/media/docs/bib-lib/pdfs/Exterior_Signage.ashx
+# Search for Parks Canada Heritage Green, and you will see that it is the
+# color on the signs, and that they give the pantone shade. I found a tool
+# online that can convert that to the hex color.
+parks_canada_heritage_green = '#284734'
