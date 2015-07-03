bc8122155029ba8b5efc376fbb6b145729aec99d
diabolique- <gillen314@gmail.com>
2015-07-03 14:36:45 -0500

    made bold axis labels, organized __init__ file

--- __init__.py
+++ __init__.py
@@ -4,6 +4,35 @@ import matplotlib as mpl
 from .tools import *
 from .colors import *
 
+
+
+mpl.rcParams['legend.scatterpoints'] = 1
+mpl.rcParams['savefig.format'] = 'pdf'
+mpl.rcParams['axes.formatter.useoffset'] = False
+mpl.rcParams['figure.autolayout'] = True
+mpl.rcParams['figure.dpi'] = 300
+
+# Font options
+mpl.rcParams['font.family'] = 'sans-serif'
+mpl.rcParams['font.sans-serif'] = 'Helvetica Neue'
+mpl.rcParams['font.weight'] = 'bold'
+mpl.rcParams['axes.labelweight'] = 'bold'
+mpl.rcParams['axes.titleweight'] = 'bold'
+mpl.rcParams['font.size'] = 14
+mpl.rcParams['axes.labelsize'] = 14
+mpl.rcParams['xtick.labelsize'] = 12
+mpl.rcParams['ytick.labelsize'] = 12
+mpl.rcParams['legend.fontsize'] = 14
+
+# colors
+mpl.rcParams['patch.edgecolor'] = almost_black
+mpl.rcParams['text.color'] = almost_black
+mpl.rcParams['axes.edgecolor'] = almost_black
+mpl.rcParams['axes.labelcolor'] = almost_black
+mpl.rcParams['xtick.color'] = almost_black
+mpl.rcParams['ytick.color'] = almost_black
+mpl.rcParams['grid.color'] = almost_black
+# I like my own color cycle based on one of the Tableu sets.
 mpl.rcParams['axes.color_cycle'] = [Tableau_10.hex_colors[3],
                                     Tableau_10.hex_colors[0],
                                     Tableau_10.hex_colors[2],
@@ -14,22 +43,7 @@ mpl.rcParams['axes.color_cycle'] = [Tableau_10.hex_colors[3],
                                     Tableau_10.hex_colors[9],
                                     Tableau_10.hex_colors[8],
                                     Tableau_10.hex_colors[5]]
-mpl.rcParams['legend.scatterpoints'] = 1
-mpl.rcParams['savefig.format'] = 'pdf'
-mpl.rcParams['font.family'] = 'sans-serif'
-mpl.rcParams['font.sans-serif'] = 'Helvetica Neue'
-mpl.rcParams['font.weight'] = 'bold'
-mpl.rcParams['font.size'] = 14
-mpl.rcParams['axes.labelsize'] = 14
-mpl.rcParams['xtick.labelsize'] = 12
-mpl.rcParams['ytick.labelsize'] = 12
-mpl.rcParams['legend.fontsize'] = 14
-mpl.rcParams['patch.edgecolor'] = almost_black
-mpl.rcParams['text.color'] = almost_black
-mpl.rcParams['axes.edgecolor'] = almost_black
-mpl.rcParams['axes.labelcolor'] = almost_black
-mpl.rcParams['axes.labelweight'] = 'bold'
-mpl.rcParams['axes.titleweight'] = 'bold'
-mpl.rcParams['xtick.color'] = almost_black
-mpl.rcParams['ytick.color'] = almost_black
-mpl.rcParams['grid.color'] = almost_black
+
+# TODO: make errorbar plot where the errorbars look nicer when there are a ton
+# of points. Maybe try to make the errorbars themselves transparent slightly,
+# or try to make sure they are always behind the actual points.
