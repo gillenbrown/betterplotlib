ad5885ff5e587f3ccebaabf6878feb76a1552a0d
diabolique- <gillen314@gmail.com>
2015-08-10 18:55:33 -0500

    moved color cycle
    made it accessible to the user if they want to grab colors from it.

--- prettyplot/__init__.py
+++ prettyplot/__init__.py
@@ -1,4 +1,3 @@
-from palettable.tableau import Tableau_10
 import matplotlib as mpl
 
 from .tools import *
@@ -33,16 +32,7 @@ mpl.rcParams['xtick.color'] = almost_black
 mpl.rcParams['ytick.color'] = almost_black
 mpl.rcParams['grid.color'] = almost_black
 # I like my own color cycle based on one of the Tableu sets.
-mpl.rcParams['axes.color_cycle'] = [Tableau_10.hex_colors[3],
-                                    Tableau_10.hex_colors[0],
-                                    Tableau_10.hex_colors[2],
-                                    Tableau_10.hex_colors[4],
-                                    Tableau_10.hex_colors[1],
-                                    Tableau_10.hex_colors[6],
-                                    Tableau_10.hex_colors[7],
-                                    Tableau_10.hex_colors[9],
-                                    Tableau_10.hex_colors[8],
-                                    Tableau_10.hex_colors[5]]
+mpl.rcParams['axes.color_cycle'] = color_cycle
 
 # TODO: make errorbar plot where the errorbars look nicer when there are a ton
 # of points. Maybe try to make the errorbars themselves transparent slightly,
--- prettyplot/colors.py
+++ prettyplot/colors.py
@@ -1,3 +1,5 @@
+from palettable.tableau import Tableau_10
+
 almost_black = '#262626'
 light_gray = '#E5E5E5'
 steel_blue = '#3F5D7D'
@@ -8,3 +10,14 @@ steel_blue = '#3F5D7D'
 # color on the signs, and that they give the pantone shade. I found a tool
 # online that can convert that to the hex color.
 parks_canada_heritage_green = '#284734'
+
+color_cycle = [Tableau_10.hex_colors[3],
+               Tableau_10.hex_colors[0],
+               Tableau_10.hex_colors[2],
+               Tableau_10.hex_colors[4],
+               Tableau_10.hex_colors[1],
+               Tableau_10.hex_colors[6],
+               Tableau_10.hex_colors[7],
+               Tableau_10.hex_colors[9],
+               Tableau_10.hex_colors[8],
+               Tableau_10.hex_colors[5]]
