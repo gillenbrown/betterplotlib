e7ced1c9ace84df89f3a4648f31f8ed53e8f799f
gillenbrown <gillenbrown@gmail.com>
2016-05-27 12:09:10 -0500

    added things to be uploadable to pypi, changed gitignore

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+	name="betterplotlib",
+
+	version="0.1.0",
+
+	description="Some wrappers for matplotlib to make plotting easier and nicer.",
+	long_description="This module contains wrapper functions for matplotlib. A lot of the matplotlib plots are ugly and not easy to make, so I wrote some functions that do a lot of the stuff that should be easy, as well as wrappers for common plots that make them look nicer. ",
+
+	url="http://betterplotlib.readthedocs.io/en/latest/",
+
+	author="Gillen Brown",
+	author_email="gillenbrown@gmail.com",
+
+	license="GNU GPLv3",
+
+	keywords="plotting matplotlib",
+
+	packages=find_packages(exclude=["docs"]),
+
+	install_requires=["matplotlib",  "numpy",  "palettable"]
+
+	)
