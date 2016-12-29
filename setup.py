1fa11d544c85dbdc18058fddb71afd2207721738
Gillen Brown <gillenbrown@gmail.com>
2016-12-28 22:09:43 -0600

    updated for PyPI upload

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 setup(
 	name="betterplotlib",
 
-	version="0.4.2",
+	version="1.0.1",
 
 	description="Some wrappers for matplotlib to make plotting easier and nicer.",
 	long_description="This module contains wrapper functions for matplotlib. A lot of the matplotlib plots are ugly and not easy to make, so I wrote some functions that do a lot of the stuff that should be easy, as well as wrappers for common plots that make them look nicer. ",
 
-	url="http://betterplotlib.readthedocs.io/en/latest/",
+	url="http://betterplotlib.readthedocs.io/en/master/",
 
 	author="Gillen Brown",
 	author_email="gillenbrown@gmail.com",
