98ecde6bbb9830b640c317074121989cb5b4a016
Gillen Brown <gillenbrown@gmail.com>
2019-12-14 15:38:36 -0500

    added color fading functions

--- betterplotlib/colors.py
+++ betterplotlib/colors.py
@@ -1,4 +1,5 @@
 import palettable
+from matplotlib import colors as mpl_colors
 
 almost_black = '#262626'
 light_gray = '#E5E5E5'
@@ -25,3 +26,28 @@ color_cycle = [color_cycle_map[5],
                color_cycle_map[9],
                color_cycle_map[7]
               ]
+
+# Some functions to fade and unfade colors, which can be helpful when plotting
+# two lines with similar meaning
+def fade_color(color):
+    rgb = mpl_colors.to_rgb(color)
+    hsv = mpl_colors.rgb_to_hsv(rgb)
+    h = hsv[0]
+    s = hsv[1] / 3.0  # remove saturation
+    # make things lighter - 3/4 of the way to full brightness. In combination
+    # with the reduction in saturation, it basically fades things whiter
+    v = hsv[2] + (1.0 - hsv[2]) * 0.75
+
+    return mpl_colors.hsv_to_rgb([h, s, v])
+
+def unfade_color(color):
+    rgb = mpl_colors.to_rgb(color)
+    hsv = mpl_colors.rgb_to_hsv(rgb)
+    h = hsv[0]
+    s = hsv[1] * 3.0 # restore saturation
+    # earlier we went 3/4 of the way to full brighness. So to restore it, we
+    # need to take this down 3 times the different between the current v and
+    # full v
+    v = hsv[2] - (3 * (1.0 - hsv[2]))
+
+    return mpl_colors.hsv_to_rgb([h, s, v])
